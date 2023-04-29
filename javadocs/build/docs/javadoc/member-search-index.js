2c9efed3c0773af2925e6fbb5bce37c1c8a652aa
Enrique Zavala <enrique.zavala@connecta.mx>
2023-04-28 19:35:20 -0600

    add all files examples

---
+++ javadocs/build/docs/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"mx.connecta","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"mx.connecta","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
