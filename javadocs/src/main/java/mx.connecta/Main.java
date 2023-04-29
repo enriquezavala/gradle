601e757adb5c6cd8ff8f10e4d26f03d1463155a6
Enrique Zavala <enrique.zavala@connecta.mx>
2023-04-28 19:27:03 -0600

    add javadocs example

---
+++ javadocs/src/main/java/mx.connecta/Main.java
@@ -0,0 +1,14 @@
+package mx.connecta;
+/**
+ * The Main class implements an application that
+ * simply displays "Hello Gradle" to the standard output.
+ */
+public class Main {
+    /**
+     * starts the application
+     * @param args the command line parameters
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle");
+    }
+}
