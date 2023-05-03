f3fec99ac684bdc9e3c4276612bd3d8dc2affbd8
Enrique Zavala <enrique.zavala@connecta.mx>
2023-05-03 06:42:59 -0600

    add remote-repository example

---
+++ remote-repository/src/main/java/mx/connecta/Main.java
@@ -0,0 +1,11 @@
+package mx.connecta;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    final static Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello Gradle");
+    }
+}
